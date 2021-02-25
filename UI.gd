edcd5202da001d959b94479d2489a5bf6fc09397
jrh-w <jakub.r.wlodarz@gmail.com>
2021-02-25 22:12:42 +0100

    Rect's added

--- UI.gd
+++ UI.gd
@@ -7,12 +7,15 @@ extends Control
 
 onready var container = get_node("HBoxContainer/VBoxContainer/roundInfo/HBoxContainer")
 onready var new_box = load("res://roundBlock.tscn")
+onready var new_shape = load("res://shapeShell.tscn")
+
+onready var square = load("res://colorPalette/box.png")
 
 var colors = ["green", "blue", "yellow"]
 var shapes = ["box", "circle", "Hexagon", "square", "triangle"]
 
 var colorTable = [[1, 2, 3], [1, 3, 3], [1, 1, 2]]
-var shapeTable = [1, 3, 5]
+var shapeTable = [1, 3, 5, 2]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,14 +23,17 @@ func _ready():
 	pass # Replace with function body.
 
 func get_colors(colorTab = colorTable, shapeTab = shapeTable):
-	var shape = null
+	var box = null
 	for colors in colorTab:
 		print("hi")
-		shape = new_box.instance()
+		box = new_box.instance()
 		for shapes in shapeTable:
-			#var new_shape = 
+			var shape = new_shape.instance()
+			shape.texture = square
+			box.get_node("MarginContainer/HBoxContainer").add_child(shape)
+			#shape.get_node()
 			pass
-		container.add_child(shape)
+		container.add_child(box)
 		
 
 
