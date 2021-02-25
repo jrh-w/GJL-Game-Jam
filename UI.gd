6d4f479c4c2e5dbacc42141a9dea5c70f6c2c00c
jrh-w <jakub.r.wlodarz@gmail.com>
2021-02-25 22:33:03 +0100

    Color added

--- UI.gd
+++ UI.gd
@@ -9,13 +9,16 @@ onready var container = get_node("HBoxContainer/VBoxContainer/roundInfo/HBoxCont
 onready var new_box = load("res://roundBlock.tscn")
 onready var new_shape = load("res://shapeShell.tscn")
 
-onready var square = load("res://colorPalette/box.png")
+onready var square = load("res://colorPalette/square.png")
+onready var circle = load("res://colorPalette/circle.png")
+onready var Hexagon = load("res://colorPalette/Hexagon.png")
+onready var triangle = load("res://colorPalette/triangle.png")
 
 var colors = ["green", "blue", "yellow"]
-var shapes = ["box", "circle", "Hexagon", "square", "triangle"]
+var shapes = ["square", "circle", "Hexagon", "triangle"]
 
 var colorTable = [[1, 2, 3], [1, 3, 3], [1, 1, 2]]
-var shapeTable = [1, 3, 5, 2]
+var shapeTable = [0, 2, 3, 1]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,11 +28,11 @@ func _ready():
 func get_colors(colorTab = colorTable, shapeTab = shapeTable):
 	var box = null
 	for colors in colorTab:
-		print("hi")
 		box = new_box.instance()
-		for shapes in shapeTable:
+		for number in shapeTable:
 			var shape = new_shape.instance()
-			shape.texture = square
+			shape.get_node("Sprite").texture = load("res://colorPalette/" + shapes[number] + ".png")
+			shape.get_node("Sprite").modulate = Color(0, 0, 1, 1)
 			box.get_node("MarginContainer/HBoxContainer").add_child(shape)
 			#shape.get_node()
 			pass
