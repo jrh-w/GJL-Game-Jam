e0e88e798d6ade7b935cae133fa9547e74a8f1a3
devoxi <sim28888@gmail.com>
2021-02-26 20:37:07 +0100

    Dodaj wiadomość comittu

--- dropzone.gd
+++ dropzone.gd
@@ -2,10 +2,30 @@ extends Position2D
 
 var busy = false
 var color = '#8fc6e0'
+var function = 'default'
+
+func _enter_tree():
+	if function == "back":
+		print("go back")
+	elif function == "skip":
+		print("skip")
+	elif function == "stop":
+		print("stop")
 
 func select():
 	$box2.modulate = color
 	busy = true
+	
+	if function == "back":
+		print("go back")
+		$box.texture = load("res://colorPalette/box_background_back.png")
+	elif function == "skip":
+		$box.texture = load("res://colorPalette/box_background_skip.png")
+		print("skip")
+	elif function == "stop":
+		$box.texture = load("res://colorPalette/box_background_stop.png")
+		print("stop")
+	
 	
 func deselect():
 	busy = false
