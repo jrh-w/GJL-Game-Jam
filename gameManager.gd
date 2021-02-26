289cbc12ad05ecd4119feb1f9e6c7b8e610c8408
jrh-w <jakub.r.wlodarz@gmail.com>
2021-02-26 22:43:08 +0100

    Scene restored

--- gameManager.gd
+++ gameManager.gd
@@ -9,8 +9,6 @@ var history = [] # History of our moves
 
 var roundEnd = false
 
-var paused = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -19,13 +17,11 @@ func new_log(name, a, b):
 	var n = {
 		"card": name,
 		"from": a,
-		"to": b,
-		"doneWhilePaused": paused
+		"to": b
 	}
 	# Such is the needed transition between the blocks
 	if !roundEnd: 
-		if !paused:
-			get_node("UI/TextureRect").offset += 93
+		get_node("UI/TextureRect").offset += 93
 		history.push_front(n)
 		
 func restart_round():
@@ -34,18 +30,16 @@ func restart_round():
 			var route = history.pop_front()
 			get_node("innerGame/" + route.card).rest_point.deselect()
 			get_node("innerGame/" + route.card).rest_point = route.from
-			if !route.doneWhilePaused:
-				get_node("UI/TextureRect").offset -= 93
-				
-func backOneRound():
-	if !roundEnd && history.size() > 1:
-			var route1 = history.pop_front()
-			var route2 = history.pop_front()
-			get_node("innerGame/" + route2.card).rest_point.deselect()
-			get_node("innerGame/" + route2.card).rest_point = route2.from
-			history.push_front(route1)
-			if !paused:
-				get_node("UI/TextureRect").offset -= 93
+			get_node("UI/TextureRect").offset -= 93
+
+func reverse_round():
+	if !roundEnd && history.size() > 0:
+		var route = history.pop_front()
+		get_node("innerGame/" + route.card).rest_point.deselect()
+		get_node("innerGame/" + route.card).rest_point = route.from
+		get_node("UI/TextureRect").offset -= 93
+	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
