88ce8f7fbc8722227b9366bd5dfde31fd6eed43a
jrh-w <jakub.r.wlodarz@gmail.com>
2021-02-27 14:52:45 +0100

    Menu changes

---
+++ menu.gd
@@ -0,0 +1,23 @@
+extends MarginContainer
+
+onready var mainMenu = get_node("CenterContainer/mainMenu")
+onready var settingsMenu = get_node("CenterContainer/settingsMenu")
+onready var levelMenu = get_node("CenterContainer/levelMenu")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_settingsButton_pressed():
+	mainMenu.hide()
+	settingsMenu.show()
+
+
+func _on_Button_button_down():
+	mainMenu.hide()
+	levelMenu.show()
+
+
+func _on_backButton_pressed():
+	settingsMenu.hide()
+	mainMenu.show()
---
+++ settingsMenu.gd
@@ -0,0 +1,49 @@
+extends VBoxContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var optionMenu = get_node("HBoxContainer/optionMenu")
+onready var fullscreenOption = get_node("fullscreen/isFullscreen")
+
+var selected
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_resolutions()
+	optionMenu.connect("item_selected", self, "change_resolution")
+
+func add_resolutions():
+	optionMenu.add_item("1024x600", 0)
+	optionMenu.add_item("1280x720", 1)
+	optionMenu.add_item("1366x768", 2)
+	optionMenu.add_item("1440x900", 3)
+	optionMenu.add_item("1600x900", 4)
+	optionMenu.add_item("1920x1080", 5)
+	pass
+
+func change_resolution(id):
+	selected = id
+	if id == 0:
+		OS.set_window_size(Vector2(1024, 600))
+	elif id == 1:
+		OS.set_window_size(Vector2(1280, 720))
+	elif id == 2:
+		OS.set_window_size(Vector2(1366, 768))
+	elif id == 3:
+		OS.set_window_size(Vector2(1440, 900))
+	elif id == 4:
+		OS.set_window_size(Vector2(1600, 900))
+	elif id == 5:
+		OS.set_window_size(Vector2(1920, 1080))
+	OS.set_window_position(Vector2(0, 0))
+
+func _on_isFullscreen_pressed():
+	if fullscreenOption.pressed:
+		OS.set_window_fullscreen(true)
+		optionMenu.disabled = true
+	else:
+		OS.set_window_fullscreen(false)
+		optionMenu.disabled = false
